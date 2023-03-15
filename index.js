22e32ed72b2f937121ae47fea0bee5f7f0b7a157
Dipesh-J <dipeshjoshi015@gmail.com>
2023-03-16 00:59:39 +0530

    Final Push

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(cors({
+    origin: ["http://localhost:3001", "https://input-box-backend.vercel.app/"]
+}))
+
+// Add the middleware function here
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 mongoose.connect("mongodb+srv://group22:1234@group22databse.uvtoalh.mongodb.net/input-box", { useNewUrlParser:true})
 .then(()=> console.log("Connected to MongoDb"))
 .catch((err)=> console.error(err));
