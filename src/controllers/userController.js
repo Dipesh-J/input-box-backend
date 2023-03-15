376771dd95b89e29ec83599c6c24d7a61c6611b7
Dipesh-J <dipeshjoshi015@gmail.com>
2023-03-16 00:28:01 +0530

    some changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,19 +8,26 @@ module.exports = {
       const { name, email, age, gender, password } = req.body;
       const existingUser = await userModel.findOne({ email });
       if (existingUser) {
-        return res.status(409).json({ message: "User already exists" });
+        console.log("User Already exists");
+        return res.status(409).send({ message: "User already exists" });
       }
 
       // Hash the password using bcrypt
       const hashedPassword = await bcrypt.hash(password, 10);
 
       // Create a new user document with the hashed password
-      const newUser = new userModel({ name, email, password: hashedPassword, age, gender,});
+      const newUser = new userModel({
+        name,
+        email,
+        password: hashedPassword,
+        age,
+        gender,
+      });
       await newUser.save();
-      return res.status(201).json({ status: true, message: "Account Created" });
-    } 
-    catch (error) {
-      return res.status(500).json({ status: false, message: error.message });
+      return res.status(201).send({ status: true, message: "Account Created" });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).send({ status: false, message: error.message });
     }
   },
   login: async (req, res) => {
@@ -42,6 +49,7 @@ module.exports = {
       // validation starts
 
       if (!req.body.email || !req.body.password) {
+        console.log("both fields mandatory");
         return res.status(400).send({
           status: false,
           message: "both fields are required to sign in.",
@@ -50,25 +58,32 @@ module.exports = {
 
       // Checking if the user already exists in Db or not
       const userCheck = await userModel.findOne({ email: email });
-      if (!userCheck)
+      if (!userCheck) {
+        console.log("not registered");
         return res
           .status(404)
-          .json({
+          .send({
             status: false,
             message:
               "you are not registered. first signup then try to sign in again.",
           });
+      }
+
       // CHECKING THE PASSWORD
       let passwordCheck = await bcrypt.compare(password, userCheck.password);
-      if (!passwordCheck)
-        return res.status(400).send({ status: false, message: "Incorrect Password" });
+      if (!passwordCheck) {
+        console.log("Incorrect password");
+        return res
+          .status(400)
+          .send({ status: false, message: "Incorrect Password" });
+      }
 
       // Create a JWT token with the user's ID and send it in the response
       const token = jwt.sign({ userId: userCheck._id }, "secret");
-      return res.status(200).json({ message: "User logged In", token: token });
-    } 
-    catch (error) {
-      return res.status(500).json({ status: false, message: error.message });
+      res.setHeader("x-api-key", token);
+      return res.status(200).send({ message: "User logged In", token: token });
+    } catch (error) {
+      return res.status(500).send({ status: false, message: error.message });
     }
   },
 };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,13 @@
-const router = require('express').Router();
-const { addText } = require('../controllers/textController')
-const { signUp, login } = require('../controllers/userController')
+const router = require("express").Router();
+const { addText } = require("../controllers/textController");
+const { signUp, login } = require("../controllers/userController");
 // const { authentication } = require('../middlewares/auth')
 
-router.post('/addText', addText)
-router.post('/signUp', signUp)
-router.post('/login', login)
+router.post("/addText", addText);
+router.post("/signup", signUp);
+router.post("/login", login);
 
+router.all("/*", async function (req, res) {
+  return res.status(404).send({ status: false, message: "Page Not Found." });
+});
 module.exports = router;
