97d9e3fd38a83264e57c589de21f949aff5bac4e
Dipesh-J <dipeshjoshi015@gmail.com>
2023-03-16 03:01:53 +0530

    try

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 //     next();
 //   });
   app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://velvety-lolly-85a0f2.netlify.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
